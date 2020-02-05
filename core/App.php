151d72b5e6447ca84f53d5a6e2f698f3492f2e78
THE JOKER <pavel11_06@mail.ru>
2020-02-05 16:46:46 +0800

    core

---
+++ core/App.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace core;
+
+/**
+ * Ядро системы
+ *
+ * @return void
+ */
+class App
+{
+    /**
+     * Запуск проекта
+     *
+     * @throws \ErrorException
+     */
+    public static function run()
+    {
+        /**
+         * Парсинг Url
+         *
+         */
+        $path = $_SERVER['REQUEST_URI'];
+        $pathParts = explode('/', $path);
+        $controller = $pathParts[1];
+        $action = $pathParts[2];
+
+        /**
+         * Пространство имен для контроллеров и именование действий
+         *
+         */
+        $controller = 'controllers\\' . $controller . 'Controller';
+        $action = 'action' . ucfirst($action);
+
+        if (!class_exists($controller)) {
+            throw new \ErrorException('Controller does not exist');
+        }
+        
+        /**
+         * Создаем объект контроллера
+         *
+         */
+        $objController = new $controller;
+
+        if (!method_exists($objController, $action)) {
+            throw new \ErrorException('action does not exist');
+        }
+
+        $objController->$action();
+    }
+}
